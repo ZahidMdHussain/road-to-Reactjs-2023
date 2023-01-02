bec1aa86d58a2a3cfff23da8b820ccafd78be557
Zahid <zahimdhussain786@gmail.com>
2023-01-02 18:11:02 +0530

    Ch-02 igniting App

--- Ch-02_Igniting-App/App.js
+++ Ch-02_Igniting-App/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+let i=0;
+function changeBg(){
+  const bgcolor = ['#D7BDE2', '#F4D03F','#F5B7B1','#A9DFBF','#CCD1D1'];
+  document.getElementById('root').style.backgroundColor = bgcolor[i];
+  i++;
+  if(i>bgcolor.length-1)
+    i=0;
+}
+
 const heading1 = React.createElement(
     "h1",
     {
@@ -26,3 +35,4 @@ const heading1 = React.createElement(
   );
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(container);
+  setInterval(changeBg,2000);
