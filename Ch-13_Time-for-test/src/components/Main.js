66276e842f5a9a49f7ed1c0029e590dfaaeae2f0
Zahid <zahimdhussain786@gmail.com>
2023-02-10 15:01:16 +0530

    Ch-13 Time for some testing

--- Ch-13_Time-for-test/src/components/Main.js
+++ Ch-13_Time-for-test/src/components/Main.js
@@ -1,70 +1,99 @@
-import React, {useState, useContext, useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
-import './main.css';
-import Shimmer, {NoRestaurant} from './Shimmer';
-import userContext from '../utils/LoginContext';
+import React, { useState, useContext, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./main.css";
+import Shimmer, { NoRestaurant } from "./Shimmer";
+import userContext from "../utils/LoginContext";
 // import useAllResturants from "../utils/useAllResturants"
-import Card from './Card';
-import Login from './Login';
-import {Link} from 'react-router-dom';
-import {filterSerachCard} from '../utils/helper';
+import Card from "./Card";
+import Login from "./Login";
+import { Link } from "react-router-dom";
+import { filterSerachCard } from "../utils/helper";
 import useOnline from "../utils/useOnline";
 
-function Main () {
-    const [searchText, setSearchText] = useState("");
-    const [filteredRestaurants, setFilteredRestaurant] = useState([]);
-    const [allRestaurants, setAllRestaurant] = useState([]);
-    
-    // intialize useEffect
+function Main() {
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurants, setFilteredRestaurant] = useState([]);
+  const [allRestaurants, setAllRestaurant] = useState([]);
 
-    useEffect(() => {
-        getApiData()
-    },[])
+  // intialize useEffect
 
-    // api call functions
-    async function getApiData(){
-        const data = await fetch ('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
-        const json = await data.json();
-        setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
-        setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
-    }
+  useEffect(() => {
+    getApiData();
+  }, []);
 
-    const {login} = useContext(userContext);
-
-    const isOnline = useOnline();
-    if (!isOnline){
-        return <h2>🔴It seems your internet connection lost...</h2>
-    }
-
-    return (!allRestaurants)?null:(
-        <div className='main'>
-            <div className='sub-head'>
-                <p>Top restaurants of day</p>
-                <Login />
-                <h3>{login.username}</h3>
-                <div className='search-conatiner'>
-                <input type="text" className='search-input' placeholder="Search here.." value={searchText} onChange={ (e) => {
-                    setSearchText(e.target.value)
-                }}/>
-                <button className='serach-btn' onClick={() => {
-                    const filteredRestaurants = filterSerachCard(searchText, allRestaurants);
-                    setFilteredRestaurant(filteredRestaurants);
-                }}>Search</button>
-                </div>  
-            </div>
-            <div className='card-container'>    
-            {(allRestaurants.length===0?<Shimmer />:
-            filteredRestaurants.length===0?<NoRestaurant />:
-            filteredRestaurants.map((each_resturant) =>{
-                return  <Link className='card' to={"/restaurants/"+each_resturant?.data?.id} key={each_resturant?.data?.id} >
-                <Card {...each_resturant?.data} />
-                </Link>
-            })
-            )}
-            
-            </div>
-        </div>
+  // api call functions
+  async function getApiData() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
+    const json = await data.json();
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  const { login } = useContext(userContext);
+
+  const isOnline = useOnline();
+  if (!isOnline) {
+    return (
+      <h2 data-testid="checkOnline">
+        🔴It seems your internet connection lost...
+      </h2>
+    );
+  }
+
+  return !allRestaurants ? null : (
+    <div className="main">
+      <div className="sub-head">
+        <p>Top restaurants of day</p>
+        <Login />
+        <h3>{login.username}</h3>
+        <div className="search-conatiner">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search here.."
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            data-testid="checkSearch"
+            className="serach-btn"
+            onClick={() => {
+              const filteredRestaurants = filterSerachCard(
+                searchText,
+                allRestaurants
+              );
+              setFilteredRestaurant(filteredRestaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="card-container">
+        {allRestaurants.length === 0 ? (
+          <Shimmer />
+        ) : filteredRestaurants.length === 0 ? (
+          <NoRestaurant />
+        ) : (
+          filteredRestaurants.map((each_resturant) => {
+            return (
+              <Link
+                className="card"
+                to={"/restaurants/" + each_resturant?.data?.id}
+                key={each_resturant?.data?.id}
+              >
+                <Card {...each_resturant?.data} />
+              </Link>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Main;
--- Ch-13_Time-for-test/src/components/__tests__/Online.test.js
+++ Ch-13_Time-for-test/src/components/__tests__/Online.test.js
@@ -1,11 +1,42 @@
-import Main from "../Main"
-import {render} from "@testing-library/react"
+import Main from "../Main";
+import { getByTestId, render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../../utils/store";
-import {StaticRouter} from "react-router-dom/server"
-test ("Test Online/Offline features", () => {
-    //Load Body
-    const body = render(
+import { StaticRouter } from "react-router-dom/server";
+import { restaurantData } from "../../mocks/data";
+
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(restaurantData);
+    },
+  });
+});
+
+test("Test Online/Offline features", () => {
+  //Load Body
+  const body = render(
+    <StaticRouter>
+      <Provider store={store}>
         <Main />
-    )
+      </Provider>
+    </StaticRouter>
+  );
+  // Is online testing
+  const online = body.getByTestId("checkOnline");
+  expect(online.innerHTML).toBe("");
+});
+
+test("Test Serach features on HomePage", () => {
+  //Load Body
+  const body = render(
+    <StaticRouter>
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    </StaticRouter>
+  );
+  // Is online testing
+  const searchbtn = body.getByTestId("checkSearch");
+  expect(searchbtn.innerHTML).toBe("Search");
 });
