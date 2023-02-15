fc66309551c39a45d9e7bb4af2d6a042ae01fac6
Zahid <zahimdhussain786@gmail.com>
2023-02-15 15:42:33 +0530

    Final version of Late Night Bite app

--- Final-Project/src/components/Login.js
+++ Final-Project/src/components/Login.js
@@ -4,25 +4,33 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   const { login, setlogin } = useContext(userContext);
+
   return (
     <>
-      <div>
-        <div>
-          <input
-            onChange={(e) => {
-              setlogin({
-                username: e.target.value,
-              });
-            }}
-            className=""
-            type="text"
-            name="login"
-            placeholder="Username"
-          />
-        </div>
-        <Link to="/">
-          <button>Submit</button>
-        </Link>
+      <div className="h-[60vh] flex justify-center items-center bg-slate-300">
+        <form className="border border-gray-600 p-5">
+          <div className="my-2">
+            <input
+              onChange={(e) => {
+                setlogin({
+                  username: e.target.value,
+                });
+              }}
+              className=""
+              type="text"
+              name="name"
+              placeholder="Firstname"
+            />
+          </div>
+          <div className="my-2">
+            <input className="" type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="bg-yellow-400" type="submit">
+            <Link to="/">
+              <button type="submit">Submit</button>
+            </Link>
+          </div>
+        </form>
       </div>
     </>
   );
