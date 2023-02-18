b1455803207a848fdd7ed5fd3ca8d7e06d96eb89
Zahid <zahimdhussain786@gmail.com>
2023-02-18 18:39:53 +0530

    Building own video streaming app

--- video-streamimg-app/src/components/Header.js
+++ video-streamimg-app/src/components/Header.js
@@ -11,28 +11,40 @@ const Header = () => {
     <div className="grid grid-flow-col py-2 px-4 shadow-lg">
       <div className="flex items-center col-span-1">
         <img
-          className="h-[32px] px-2"
+          className="h-[38px] mx-2 p-2 hover:rounded-full hover:bg-gray-200 hover:cursor-pointer"
           src={hamburg}
           alt="sidebar-toggle-button"
         />
-        <img className="h-[25px]" src={logo} alt="app-logo" />
+        <img className="h-[24px]" src={logo} alt="app-logo" />
       </div>
       <div className="col-span-10 text-center">
         <input
-          className="w-2/4 p-2 px-6 border border-slate-400 rounded-tl-full rounded-bl-full outline-blue-400"
+          className="w-2/4 p-2 px-6 border border-slate-400 rounded-tl-full rounded-bl-full outline-blue-200"
           type="text"
           name=""
           id=""
           placeholder="Serach"
         />
-        <button className="p-2 border bg-gray-100 border-l-0  border-slate-400 rounded-tr-full rounded-br-full align-top hover:bg-gray-200">
-          <img className="h-[24px] px-2" src={search} alt="" />
+        <button className="p-2 border bg-gray-50 border-l-0  border-slate-400 rounded-tr-full rounded-br-full align-top hover:bg-gray-200">
+          <img className="h-[24px] px-3" src={search} alt="" />
         </button>
       </div>
       <div className="flex justify-center items-center col-span-1">
-        <img className="h-[30px] px-2 ml-4" src={video} alt="" />
-        <img className="h-[30px] px-2 ml-4" src={notify} alt="" />
-        <img className="h-[30px] px-2 ml-4" src={user} alt="" />
+        <img
+          className="h-[40px] p-2 ml-4 hover:rounded-full hover:bg-gray-200 hover:cursor-pointer"
+          src={video}
+          alt=""
+        />
+        <img
+          className="h-[36px] p-2 ml-4 hover:rounded-full hover:bg-gray-200 hover:cursor-pointer"
+          src={notify}
+          alt=""
+        />
+        <img
+          className="h-[36px] p-2 ml-4 hover:rounded-full hover:bg-gray-200 hover:cursor-pointer"
+          src={user}
+          alt=""
+        />
       </div>
     </div>
   );
