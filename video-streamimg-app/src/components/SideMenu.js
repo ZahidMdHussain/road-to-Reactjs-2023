40d9800d8194dab1c15286ca810b62ba5d86afd9
Zahid <zahimdhussain786@gmail.com>
2023-02-19 14:44:26 +0530

    Building own video streaming app

--- video-streamimg-app/src/components/SideMenu.js
+++ video-streamimg-app/src/components/SideMenu.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { sidemenu } from "../utils/config";
 
 const SideMenu = () => {
-  return <div>SideMenu</div>;
+  return (
+    <div>
+      <div>
+        {sidemenu.map((item) => {
+          return (
+            <div className="flex" key={item.menuId}>
+              <img src={item.img} alt="" />
+              <span>{item.text}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default SideMenu;
---
+++ video-streamimg-app/src/utils/config.js
@@ -0,0 +1,18 @@
+import notify from "../assets/img/notification.png";
+export const sidemenu = [
+  {
+    menuId: 1,
+    img: notify,
+    text: "Home",
+  },
+  {
+    menuId: 2,
+    img: "",
+    text: "Shorts",
+  },
+  {
+    menuId: 3,
+    img: "",
+    text: "Subscriptions",
+  },
+];
