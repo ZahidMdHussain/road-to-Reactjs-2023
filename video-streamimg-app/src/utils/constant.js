5e727273c9cf8a693dc942fb8ccb7890cfab377c
Zahid <zahimdhussain786@gmail.com>
2023-03-03 17:13:32 +0530

    Video streaming app deployed

--- video-streamimg-app/src/utils/constant.js
+++ video-streamimg-app/src/utils/constant.js
@@ -8,7 +8,7 @@ export const YOUTUBE_VIDEO_API_BYID =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=";
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_RELATED_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=";
